2ae2646e13a5f728bd6ce130c1f80e61e9381e30
Oscar E. González <36247829+OscarGonzalez97@users.noreply.github.com>
2019-06-28 19:52:46 -0400

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,38 +3,44 @@
 
 using namespace std;
 
+struct node
+{
+  int data=0;
+  bool color[4]={false,false,false,false};
+};
+
 //En representaciones matematicas y computacionales se utilizan tipicamente enteros no negativos como colores.
 //Existen 4 posibles colores (1,2,3,4) para este problema
 //En cada vertice del grafo se tendra un vector de los colores que NO pueden ser, siendo asi el color restante el color que tendra ese vertice, y tambien su valor!
 //El vector tendra tamanho de 4 y si el problema tiene solucion solo utilizara 3 de estos lugares, si no tiene usara los 4 y se sabra asi que el sudoku no tiene solucion
 //Entonces el vector
 
-colores
-int colores[4]={1,2,3,4};
-
+// colores
+// int colores[4]={1,2,3,4};
+//
 class Sudoku
 {
-  struct node
-  {
-    int data;
-    bool color[4]={false,false,false,false};
-  };
-
+  //Constructor?
   list<node> tablero;
 
-  node* root;
+  public:
+  //funciones
+
 
 };
 
-
 int main()
 {
+  Sudoku sudoku;
+  //aca voy hacer por ejemplo
+  //sudoku.carga_tablero()
 
-  list<int> lista;
-  lista.assign(0,5);
-  cout<<lista.size();
 
-    return 0;
-    //para WINDOWS
-    //system("PAUSE");
+  // for (std::list<int>::iterator it = fifth.begin(); it != fifth.end(); it++)
+  // std::cout << *it << ' ';
+  //cout << "size: " << tablero.size() << '\n';
+  return 0;
+  //para WINDOWS
+  //system("PAUSE");
 }
+
